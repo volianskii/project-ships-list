4004b781a93f867339d7b45ab6bf8b59d19602d5
volianskii <dima1998vol@gmail.com>
2023-10-25 13:08:51 +0300

    wip

--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -1,3 +1,3 @@
 export const LEVELS: string[] = ['All', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-export const NATIONS: string[] = ['All', 'Japan', 'U.S.S.R.', 'France', 'Germany', 'Pan-Asia', 'Poland', 'U.K.', 'U.S.A.'];
-export const TYPES: string[] = ['All', 'Cruiser', 'Battleship', 'Aircraft Carriers', 'Ships of Poland'];
+export const NATIONS: string[] = ['All', 'Japan', 'U.S.S.R.', 'France', 'Germany', 'Pan-Asia', 'Pan-America', 'Poland', 'Spain', 'U.K.', 'Italy', 'U.S.A.'];
+export const TYPES: string[] = ['All', 'Cruiser', 'Battleship', 'Aircraft Carrier', 'Destroyer', 'Submarine'];
